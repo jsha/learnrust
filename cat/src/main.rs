2780cc291db08b682dd6dddd4e319e5a19ecd48f
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2020-05-03 23:01:16 -0700

    Handle stdin case.

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -1,33 +1,47 @@
 use std::env;
 use std::fs::File;
 use std::io;
-use std::io::stderr;
-use std::io::stdout;
 use std::io::BufRead;
 use std::io::Write;
+use std::process;
 
 fn main() {
+    if env::args().len() == 1 {
+        match copy_file_to_stdout("-", io::stdin()) {
+            Ok(()) => {}
+            Err(()) => process::exit(1),
+        }
+    }
     for arg in env::args().skip(1) {
         match copy_to_stdout(&arg) {
             Ok(()) => continue,
-            Err(()) => std::process::exit(1),
+            Err(()) => process::exit(1),
         }
     }
 }
 
 fn copy_to_stdout(filename: &str) -> Result<(), ()> {
     match File::open(filename) {
-        Ok(file) => {
-            for line in io::BufReader::new(file).lines() {
-                if let Ok(line) = line {
-                    let _ = writeln!(stdout(), "{}", line);
-                }
-            }
-            Ok(())
-        }
+        Ok(file) => copy_file_to_stdout(filename, &file),
         Err(s) => {
-            let _ = writeln!(stderr(), "{}: {}\n", filename, s);
+            let _ = writeln!(io::stderr(), "{}: {}\n", filename, s);
             Err(())
         }
     }
+}
+
+fn copy_file_to_stdout<T: io::Read>(filename: &str, file: T) -> Result<(), ()> {
+    // TODO: If there is a read error, print whatever partial line was read before the error.
+    for line in io::BufReader::new(file).lines() {
+        match line {
+            Ok(line) => {
+                let _ = writeln!(io::stdout(), "{}", line);
+            }
+            Err(s) => {
+                let _ = writeln!(io::stderr(), "{}: {}\n", filename, s);
+                return Err(());
+            }
+        }
+    }
+    Ok(())
 }
