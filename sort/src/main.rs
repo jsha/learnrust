a64bb98cb1dd8671536d0f621c639ef2e14f6d69
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2020-05-15 18:54:21 -0700

    Add -c flag to sort

--- sort/src/main.rs
+++ sort/src/main.rs
@@ -1,7 +1,11 @@
+#![feature(is_sorted)]
 use std::env;
 use std::fs::File;
 use std::io;
 use std::io::{BufRead, Write};
+
+type Line = Vec<u8>;
+type LineOrdering = dyn Fn(&Line, &Line) -> std::cmp::Ordering;
 
 fn main() -> io::Result<()> {
     let conf = match process_args() {
@@ -9,13 +13,23 @@ fn main() -> io::Result<()> {
         Err(e) => return Err(io::Error::new(io::ErrorKind::Other, e)),
     };
     let mut lines: Vec<Vec<u8>> = vec![];
-    for file in conf.non_flags {
+    for file in &(conf.non_flags) {
         let r = io::BufReader::new(File::open(file)?);
         for line in r.split(b'\n') {
             lines.push(line?);
         }
     }
-    lines.sort();
+    let comparator = make_comparator(&conf);
+
+    if conf.check_order {
+        if lines.is_sorted_by(|a, b| Some(comparator(a, b))) {
+            std::process::exit(0);
+        } else {
+            std::process::exit(1);
+        }
+    }
+
+    lines.sort_by(comparator);
     let mut last: Option<&Vec<u8>> = None;
     for line in lines.iter() {
         if conf.unique && last.is_some() && line == last.unwrap() {
@@ -28,29 +42,33 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
-struct Config {
-    unique: bool,
-    non_flags: Vec<String>,
-}
-
-impl Config {
-    fn new() -> Config {
-        Config {
-            unique: false,
-            non_flags: Vec::new(),
-        }
+fn make_comparator(conf: &Config) -> Box<LineOrdering> {
+    if conf.reverse {
+        Box::new(&|a: &Line, b: &Line| a.cmp(b))
+    } else {
+        Box::new(&|a: &Line, b: &Line| b.cmp(a))
     }
 }
 
+#[derive(Default)]
+struct Config {
+    unique: bool,
+    reverse: bool,
+    check_order: bool,
+    non_flags: Vec<String>,
+}
+
 fn process_args() -> Result<Config, String> {
-    let mut conf = Config::new();
+    let mut conf = Config::default();
 
     for arg in env::args().skip(1) {
         match arg.as_str() {
             "-u" => conf.unique = true,
+            "-r" => conf.reverse = true,
+            "-c" => conf.check_order = true,
             "-h" => {
                 return Err(format!(
-                    "Usage: {} [-u] [file1 [file2 ...]]",
+                    "Usage: {} [-u] [-r] [file1 [file2 ...]]",
                     env::args().next().unwrap_or_else(|| "sort".to_string())
                 ))
             }
